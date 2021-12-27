7e6c7d487ee6879b4e391bcd48edb255d72bb9de
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-12-28 03:44:14 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,6 +46,7 @@ async def help(event):
   )
   
 @client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
+@client.on(events.NewMessage(pattern="^@all ?(.*)"))
 async def mentionall(event):
   chat_id = event.chat_id
   if event.is_private:
